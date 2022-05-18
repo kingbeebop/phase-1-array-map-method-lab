0d6fd627d596873527a016219b35bb8b22d7f559
Brendan Ward <kingbeebop@gmail.com>
2022-05-18 10:41:15 -0400

    commit tree 705105aff47319c2be010c88d63ee8a4a9d32a80

--- index.js
+++ index.js
@@ -10,7 +10,34 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+/*
+const titleCased = () => {
+  tutorials = tutorials.map(title => {
+      let words = title.split(' ')
+      words = words.map(word => {
+        word = word.charAt(0).toUpperCase + word.substring(1)
+      })
+  })
+  return tutorials
+}
+*/
 
 const titleCased = () => {
-  return tutorials
+  let newList = []
+  tutorials.map(title => {
+    let newTitle = ''
+    let words = title.split(' ')
+    words = words.map(word => {
+      let newWord = word.charAt(0).toUpperCase() + word.substring(1)
+
+      newTitle += ' ' + newWord
+    })
+    newTitle = newTitle.substring(1)
+    //console.log(newTitle)
+    newList.push(newTitle)
+    //console.log(newList)
+})
+
+console.log(newList)
+return newList
 }
